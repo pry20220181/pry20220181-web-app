8d20c3e319a692ef0451a43bd9c06f316963f387
Bill Chavez <bill.bchavez@gmail.com>
2022-09-05 19:11:37 -0500

    show all child's schems in parent view

--- Front/front/src/app/component/list-patient/list-patient.component.ts
+++ Front/front/src/app/component/list-patient/list-patient.component.ts
@@ -12,6 +12,8 @@ import { Service } from '../../service/service';
 export class ListPatientComponent implements OnInit {
 
   ListPrint: any[] = [];
+  List: any[] = [];
+  List2: any[] = this.List;
   formModal:any
 
   constructor(private router: Router,
@@ -27,12 +29,21 @@ export class ListPatientComponent implements OnInit {
   getAllChilds(){
     this.ChildrenR.getAll().subscribe((data:any)=>{
       this.ListPrint = data.value.children
-      //console.log(data.value.children)
+      this.List = data.value.children
+      console.log("list",this.ListPrint)
     })
   }
 
   search(event:any){
-
+    this.ListPrint=[]
+    this.List2=[]
+    this.ListPrint=this.List
+    for(var i=0;i<this.List.length;i++){
+    if(String(this.List[i].dni).includes(event.target.value) ){
+      this.List2.push(this.List[i])
+    }
+  }
+  this.ListPrint=this.List2
   }
 
   toggleleModal(){
--- Front/front/src/app/component/list-vaccinated-view-p/list-vaccinated-view-p.component.ts
+++ Front/front/src/app/component/list-vaccinated-view-p/list-vaccinated-view-p.component.ts
@@ -10,34 +10,51 @@ import { VaccinationRquest } from 'src/app/request/vaccination-request';
 })
 export class ListVaccinatedViewPComponent implements OnInit {
 
-  constructor(private ChildrenR:ChildrenRquest,
-     private vaccineR:VaccinationRquest ) { }
+  constructor(private ChildrenR: ChildrenRquest,
+    private vaccineR: VaccinationRquest) { }
 
-  ListChild:any[]=[]
-  childId:any
-  dataChild:any[]=[]
+  ListChild: any[] = []
+  childId: any
+  dataChild: any[] = []
+  ListShow:any[]=[]
 
   ngOnInit(): void {
     this.getchilds()
   }
 
-  getchilds(){
-    this.ChildrenR.getbyParent().subscribe((data:any)=>{
+  getchilds() {
+    this.ChildrenR.getbyParent().subscribe((data: any) => {
       this.ListChild = data.value.children
-      console.log("Dta",data)
+      console.log("Dta", data)
     })
   }
 
-  search(data:any){
 
-  }
-
-  viewdata(){
-    this.vaccineR.getinfoChildById(this.childId).subscribe((data:any)=>{
+  viewdata() {
+    this.vaccineR.getinfoChildById(this.childId).subscribe((data: any) => {
       this.dataChild = data.value.vaccinationCard.vaccinationSchemes
-      console.log("data vacconation",data)
     })
+    setTimeout(() => {
+    this.processData()
+  }, 1500);
 
+  }
+
+  processData(){
+    this.ListShow=[]
+    for (var i = 0; i < this.dataChild.length; i++) {
+      const scheme = this.dataChild[i]
+      for (var j = 0; j < scheme.vaccines.length; j++) {
+        const vaccines = scheme.vaccines[j]
+        for (var k = 0; k < vaccines.doses.length;k++) {
+          const doses = vaccines.doses[k]
+          console.log(scheme.name, ' + ',vaccines.name,' + ')
+          //this.ListShow.push(scheme.name,vaccines.name,doses.doseId,doses.putWhen,doses.healthPersonnel,doses.observations)
+          this.ListShow.push({esquema:scheme.name,vacunas:vaccines.name,dosis:doses.doseId,fecha:doses.putWhen,doctor:doses.healthPersonnel,obs:doses.observations})
+        }
+      }
+    }
+    //console.log("datashow",this.ListShow)
   }
 
 }
